29f4af41a35e4631d4df19627c3426918c9b1674
dweitzel <dweitzel@1fda6f16-4416-0410-bfd6-867a04880151>
2010-10-12 17:30:27 +0000

    Separated the configuration of the glidein to correct areas. - Early binding goes into the submission file job.submit.template - Late binding goes into the glidein_wrapper.sh
    git-svn-id: https://campusfactory.svn.sourceforge.net/svnroot/campusfactory/campus_factory/trunk@40 1fda6f16-4416-0410-bfd6-867a04880151

--- share/glidein_jobs/glidein_wrapper.sh
+++ share/glidein_jobs/glidein_wrapper.sh
@@ -7,17 +7,10 @@ tar xzf glideinExec.tar.gz
 # Make the temporary directory
 local_dir=`mktemp -d -t -p /state/partition1/tmp/`
 
+# All late-binding configurations
 export CONDOR_CONFIG=`pwd`/glidein_condor_config
-export _condor_CONDOR_HOST=ff-grid.unl.edu
-export _condor_GLIDEIN_HOST=ff-grid.unl.edu
 export _condor_LOCAL_DIR=$local_dir
 export _condor_SBIN=`pwd`/glideinExec
-export _condor_CONDOR_ADMIN=dweitzel@ff.unl.edu
-export _condor_NUM_CPUS=1
-export _condor_UID_DOMAIN=ff.unl.edu
-export _condor_FILESYSTEM_DOMAIN=ff.unl.edu
-export _condor_MAIL=/bin/mail
-export _condor_STARTD_NOCLAIM_SHUTDOWN=1200
 
 
 if [ -e `pwd`/user_job_wrapper.sh ]
