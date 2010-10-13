3c0e20484f5ce7a5cba65f1599dfe34accbc8f0a
dweitzel <dweitzel@1fda6f16-4416-0410-bfd6-867a04880151>
2010-10-13 00:53:15 +0000

    Tagging release 0.1.1
    git-svn-id: https://campusfactory.svn.sourceforge.net/svnroot/campusfactory/campus_factory/tags/CampusFactory-0.1.1@52 1fda6f16-4416-0410-bfd6-867a04880151

---
+++ RC-0.1.0/python-lib/campus_factory/ClusterStatus.py
@@ -0,0 +1,117 @@
+
+import re
+import logging
+
+from campus_factory.Parsers import AvailableGlideins
+from campus_factory.Parsers import IdleGlideins
+from campus_factory.Parsers import IdleJobs
+from campus_factory.Parsers import RunExternal
+
+
+class Singleton(type):
+    def __init__(cls, name, bases, dict):
+        super(Singleton, cls).__init__(name, bases, dict)
+        cls.instance = None
+ 
+    def __call__(cls, *args, **kw):
+        if cls.instance is None:
+            cls.instance = super(Singleton, cls).__call__(*args, **kw)
+ 
+        return cls.instance
+
+
+class CondorConfig:
+    """
+    Singleton of the condor config (as reported by condor_config_val -dump)
+    """
+    __metaclass__ = Singleton
+    
+    def __init__(self):
+        (stdout, stderr) = RunExternal("condor_config_val -dump")
+        if len(stdout) == 0:
+            error_msg = "Unable to get any output from condor_config_val.  Is condor binaries in the path?"
+            logging.error(error_msg)
+            raise Exception(error_msg)
+        
+        # Parse the stdout
+        line_re = re.compile("([\w|\d]+)\s+=\s+(.*)\Z")
+        config_dict = {}
+        for line in stdout.split('\n'):
+            match = line_re.search(line)
+            if match == None:
+                continue
+            (key, value) = match.groups()
+            config_dict[key] = value
+        
+        logging.info("Got %s values from condor_config_val" % len(config_dict.keys()))
+        self.config_dict = config_dict
+        
+    def get(self, key):
+        """
+        @param key: string key
+        @return: string - value corresponding to key or "" if key is non-valid
+                        
+        """
+        if self.config_dict.has_key(key):
+            return self.config_dict[key]
+        else:
+            return ""
+
+
+class ClusterStatus:
+    """
+    Gather statistics on the cluster status
+    
+    
+    """
+
+    def __init__(self):
+        pass
+
+    def RunExternal(self, command):
+        """ 
+        Run an external command 
+        
+        @param command: Shell command to execute
+        @return: str - stdout from command
+        """
+        (stdin, stdout, stderr) = os.popen3(command, 'r')
+        str_stdout = stdout.read()
+        return str_stdout
+
+    def GetIdleGlideins(self):
+        """ 
+        Returns the number of glideins idle in the cluster (condor_status -avail) 
+        
+        @return: int - Number of idle glidein slots.
+        """
+        availglideins = AvailableGlideins()
+        return availglideins.GetIdle()
+
+
+    def GetIdleGlideinJobs(self):
+        """ 
+        Returns the number of glidein jobs that are idle (condor_q) 
+        
+        @return: int - Number of glidein jobs submitted but still idle.
+        """
+        idleglideins = IdleGlideins()
+        return idleglideins.GetIdle()
+
+    def GetIdleJobs(self, schedds):
+        """ 
+        Returns the number of idle user jobs (condor_q) 
+        
+        @param schedds: List [] of schedd names to check for idle jobs.
+        @return: int - Number of jobs idle
+        """
+        sumidlejobs = 0
+        for schedd in schedds:
+            idlejobs = IdleJobs(schedd)
+            schedd_idlejobs = idlejobs.GetIdle()
+            if schedd_idlejobs != None:
+                sumidlejobs += schedd_idlejobs
+
+        return sumidlejobs
+
+
---
+++ RC-0.1.0/python-lib/campus_factory/Factory.py
@@ -0,0 +1,178 @@
+
+import ConfigParser
+import sys
+import time
+import logging
+import logging.handlers
+import os
+
+from campus_factory.ClusterStatus import ClusterStatus
+from campus_factory.ClusterStatus import CondorConfig
+from campus_factory.Parsers import RunExternal
+
+class Factory:
+    """
+    The main class of the factory.  Designed to be run inside the condor scheduler.
+    
+    @author: Derek Weitzel (dweitzel@cse.unl.edu)
+    
+    """
+    def __init__(self, options):
+        """
+        Initialization function.
+        
+        @param options: A set of options in the form of an options parser
+                Required options: config - location of configuration File
+        """
+
+        # Read in the configuration file
+        self.config_file = options.config
+        self.config = ConfigParser.ConfigParser()
+        files_read = self.config.read([self.config_file])
+
+        # check if no files read in
+        if len(files_read) < 1:
+            sys.stderr.write("No configuration files found.  Location = %s\n" % self.config_file)
+            sys.exit(1)
+            
+        self._SetLogging()
+        try:
+            self.condor_config = CondorConfig()
+        except Exception:
+            logging.error("Unable to get the condor configuration.  If no condor configuration, assuming condor is not available.  Exiting...")
+            raise Exception("Unable to get the condor configuration.  If no condor configuration, assuming condor is not available.  Exiting...")
+
+    def _SetLogging(self):
+        """
+        Setting the logging level and set the logging.
+        """
+        logging_levels = {'debug': logging.DEBUG,
+                          'info': logging.INFO,
+                          'warning': logging.WARNING,
+                          'error': logging.ERROR,
+                          'critical': logging.CRITICAL}
+
+        level = logging_levels.get(self.config.get("general", "loglevel"))
+        logdirectory = self.config.get("general", "logdirectory")
+        handler = logging.handlers.RotatingFileHandler(os.path.join(logdirectory, "campus_factory.log"),
+                        maxBytes=10000000, backupCount=5)
+        root_logger = logging.getLogger()
+        root_logger.setLevel(level)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        root_logger.addHandler(handler)
+        
+        
+
+
+
+    def Start(self):
+        """ 
+        Start the Factory 
+        
+        """
+
+
+        status = ClusterStatus()
+
+        # First, daemonize?
+
+        while 1:
+            logging.info("Starting iteration...")
+
+            # Check for idle glideins (idle startd's)
+            idleslots = status.GetIdleGlideins()
+            if idleslots == None:
+                logging.info("Received None from idle glideins, going to try later")
+                self.SleepFactory()
+                continue
+            logging.debug("Idle glideins = %i" % idleslots)
+            if idleslots >= int(self.config.get("general", "MAXIDLEGLIDEINS")):
+                logging.info("Too many idle glideins")
+                self.SleepFactory()
+                continue
+
+            # Check for idle glidein jobs
+            idlejobs = status.GetIdleGlideinJobs()
+            if idlejobs == None:
+                logging.info("Received None from idle glidein jobs, going to try later")
+                self.SleepFactory()
+                continue
+            logging.debug("Queued jobs = %i" % idlejobs)
+            if idlejobs >= int(self.config.get("general", "maxqueuedjobs")):
+                logging.info("Too many queued jobs")
+                self.SleepFactory()
+                continue
+
+
+            # Check for idle jobs to flock from
+            if self.config.has_option("general", "FLOCK_FROM"):
+                schedds = self.config.get("general", "FLOCK_FROM").split(",")
+            else:
+                schedds = self.condor_config.get('FLOCK_FROM').split(",")
+                
+            idleuserjobs = status.GetIdleJobs(schedds)
+            if idleuserjobs == None:
+                logging.info("Received None from idle user jobs, going to try later")
+                self.SleepFactory()
+                continue
+            logging.debug("Idle jobs = %i" % idleuserjobs)
+            if idleuserjobs < 1:
+                logging.info("No idle jobs")
+                self.SleepFactory()
+                continue
+
+            # Got this far, so submit some glideins
+            #logging.debug("idleslots = %i, idleuserjobs = %i" % (idleslots, idleuserjobs))
+            if max([idlejobs, idleslots]) < idleuserjobs:
+                logging.info("Submtting 1 glidein")
+                self.SubmitGlideins(1)
+
+            self.SleepFactory()
+
+
+
+    def SleepFactory(self):
+        sleeptime = int(self.config.get("general", "iterationtime"))
+        logging.info("Sleeping for %i seconds" % sleeptime)
+        time.sleep(sleeptime)
+
+    def SubmitGlideins(self, numSubmit):
+        """
+        Submit numSubmit glideins.
+        
+        @param numSubmit: The number of glideins to submit.
+        """
+        # Substitute values in submit file
+        file = "share/glidein_jobs/job.submit.template"
+
+        # Submit jobs
+        for i in range(numSubmit):
+            self.SingleSubmit(file)
+
+        # Delete the submit file
+
+    def SingleSubmit(self, file):
+        """
+        Submit a single glidein job
+        
+        @param file: The file (string) to submit
+        
+        """
+        options = {"WN_TMP": self.config.get("general", "worker_tmp"), \
+                   "GLIDEIN_HOST": self.condor_config.get("CONDOR_HOST"), \
+                   "GLIDEIN_Site": self.condor_config.get("COLLECTOR_NAME")}
+        
+        if self.config.has_option("general", "GLIDEIN_Site"):
+            options["GLIDEIN_Site"] = self.config.get("general", "GLIDEIN_Site")
+        
+        options_str = ""
+        for key in options.keys():
+            options_str += " -a %s=\"%s\"" % (key, options[key])
+            
+        (stdout, stderr) = RunExternal("condor_submit %s %s" % (file, options_str))
+        logging.debug("stdout: %s" % stdout)
+        logging.debug("stderr: %s" % stderr)
+
+
+
