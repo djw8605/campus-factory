d48dac6d995b64fbde00a8420efcf06253978898
djw8605 <djw8605@1fda6f16-4416-0410-bfd6-867a04880151>
2011-05-23 15:18:47 +0000

    Updating to head
    git-svn-id: https://campusfactory.svn.sourceforge.net/svnroot/campusfactory/campus_factory/branches/RC-0.4.0@149 1fda6f16-4416-0410-bfd6-867a04880151

---
+++ trunk/python-lib/campus_factory/Factory.py
@@ -0,0 +1,287 @@
+
+import ConfigParser
+import sys
+import time
+import logging
+import logging.handlers
+import os
+
+from campus_factory.ClusterStatus import ClusterStatus
+from campus_factory.ClusterStatus import CondorConfig
+from campus_factory.Parsers import RunExternal
+from campus_factory.OfflineAds.OfflineAds import OfflineAds
+
+class Factory:
+    """
+    The main class of the factory.  Designed to be run inside the condor scheduler.
+    
+    @author: Derek Weitzel (dweitzel@cse.unl.edu)
+    
+    """
+    def __init__(self, options):
+        """
+        Initialization function.
+        
+        @param options: A set of options in the form of an options parser
+                Required options: config - location of configuration File
+        """
+        
+        self.options = options
+
+        
+    
+    def Intialize(self):
+        """
+        
+        Function to initialize the factory's variables such as configuration
+        and logging
+        """
+        # Read in the configuration file
+        self.config_file = self.options.config
+        self.config = ConfigParser.ConfigParser()
+        files_read = self.config.read([self.config_file])
+
+        # check if no files read in
+        if len(files_read) < 1:
+            sys.stderr.write("No configuration files found.  Location = %s\n" % self.config_file)
+            sys.exit(1)
+            
+        self._SetLogging()
+       
+        if  self.config.has_option("general", "useoffline") and (self.config.get("general", "useoffline").lower() == "true"):
+            self.UseOffline = True
+        else:
+            self.UseOffline = False
+        
+        try:
+            self.condor_config = CondorConfig()
+        except EnvironmentError, inst:
+            logging.exception(str(inst))
+            raise inst
+        
+        
+    def _SetLogging(self):
+        """
+        Setting the logging level and set the logging.
+        """
+        logging_levels = {'debug': logging.DEBUG,
+                          'info': logging.INFO,
+                          'warning': logging.WARNING,
+                          'error': logging.ERROR,
+                          'critical': logging.CRITICAL}
+
+        level = logging_levels.get(self.config.get("general", "loglevel"))
+        logdirectory = self.config.get("general", "logdirectory")
+        handler = logging.handlers.RotatingFileHandler(os.path.join(logdirectory, "campus_factory.log"),
+                        maxBytes=10000000, backupCount=5)
+        root_logger = logging.getLogger()
+        root_logger.setLevel(level)
+        formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
+        handler.setFormatter(formatter)
+        root_logger.addHandler(handler)
+        
+        
+    def Restart(self):
+        status = ClusterStatus()
+        
+        # Get the factory id
+        factoryID = status.GetFactoryID()
+        
+        # Hold then release the factory in the queue
+        (stderr, stdout) = RunExternal("condor_hold %s" % factoryID)
+        print "Stderr = %s" % stderr.strip()
+        #print "Stdout = %s" % stdout.strip()
+        
+        (stderr, stdout) = RunExternal("condor_release %s" % factoryID)
+        print "Stderr = %s" % stderr.strip()
+        #print "Stdout = %s" % stdout.strip()
+        
+    
+    
+    def Stop(self):
+        status = ClusterStatus()
+        
+        # Get the factory id
+        factoryID = status.GetFactoryID()
+        
+        # Remove the factory job
+        (stderr, stdout) = RunExternal("condor_rm %s" % factoryID)
+        print "Stderr = %s" % stderr.strip()
+
+
+
+    def Start(self):
+        """ 
+        Start the Factory 
+        
+        """
+        self.Intialize()
+
+        status = ClusterStatus(status_constraint="IsUndefined(Offline)")
+        offline = OfflineAds()
+
+        # First, daemonize?
+
+        while 1:
+            logging.info("Starting iteration...")
+            
+            if self.UseOffline:
+                toSubmit = offline.Update( [self.GetClusterUnique()] )
+
+            # Check for idle glideins (idle startd's)
+            idleslots = status.GetIdleGlideins()
+            if idleslots == None:
+                logging.info("Received None from idle glideins, going to try later")
+                self.SleepFactory()
+                continue
+            logging.debug("Idle glideins = %i" % idleslots)
+            if idleslots >= int(self.config.get("general", "MAXIDLEGLIDEINS")):
+                logging.info("Too many idle glideins")
+                self.SleepFactory()
+                continue
+
+            # Check for idle glidein jobs
+            idlejobs = status.GetIdleGlideinJobs()
+            if idlejobs == None:
+                logging.info("Received None from idle glidein jobs, going to try later")
+                self.SleepFactory()
+                continue
+            logging.debug("Queued jobs = %i" % idlejobs)
+            if idlejobs >= int(self.config.get("general", "maxqueuedjobs")):
+                logging.info("Too many queued jobs")
+                self.SleepFactory()
+                continue
+
+            # Get the offline ads to update.
+            if self.UseOffline:
+                num_submit = offline.GetDelinquentSites( [self.GetClusterUnique()] )
+                logging.debug("toSubmit from offline %s", str(toSubmit))
+                logging.debug("num_submit = %s\n", str(num_submit))
+                    
+                if (len(toSubmit) > 0) or num_submit[self.GetClusterUnique()]:
+                    idleuserjobs = max([ num_submit[self.GetClusterUnique()], 5 ])
+                    logging.debug("OFfline ads detected jobs should be submitted.  Idle user jobs set to %i", idleuserjobs)
+                else:
+                    logging.debug("Offline ads did not detect any matches or Delinquencies.")
+                    idleuserjobs = 0
+                
+            else:    
+                # Check for idle jobs to flock from
+                if self.config.has_option("general", "FLOCK_FROM"):
+                    schedds = self.config.get("general", "FLOCK_FROM").split(",")
+                    logging.debug("Using FLOCK_FROM from the factory config.")
+                else:
+                    schedds = self.condor_config.get('FLOCK_FROM').split(",")
+                    logging.debug("Using FLOCK_FROM from the condor configuration")
+                    
+                logging.debug("Schedds to query: %s" % str(schedds))
+                idleuserjobs = status.GetIdleJobs(schedds)
+                if idleuserjobs == None:
+                    logging.info("Received None from idle user jobs, going to try later")
+                    self.SleepFactory()
+                    continue
+                logging.debug("Idle jobs = %i" % idleuserjobs)
+                if idleuserjobs < 1:
+                    logging.info("No idle jobs")
+                    self.SleepFactory()
+                    continue
+
+            
+            # Determine how many glideins to submit
+            num_submit = self.GetNumSubmit(idleslots, idlejobs, idleuserjobs)
+            logging.info("Submitting %i glidein jobs", num_submit)
+            self.SubmitGlideins(num_submit)
+
+            self.SleepFactory()
+
+
+
+    def SleepFactory(self):
+        sleeptime = int(self.config.get("general", "iterationtime"))
+        logging.info("Sleeping for %i seconds" % sleeptime)
+        time.sleep(sleeptime)
+        
+        
+    def GetNumSubmit(self, idleslots, idlejobs, idleuserjobs):
+        """
+        Calculate the number of glideins to submit.
+        
+        @param idleslots: Number of idle startd's
+        @param idlejobs: Number of glideins in queue, but not active
+        @param idleuserjobs: Number of idle user jobs from FLOCK_FROM
+        
+        @return: int - Number of glideins to submit
+        """
+        
+        # If we have already submitted enough glideins to fufill the request,
+        # don't submit more.
+        if max([idlejobs, idleslots]) >= idleuserjobs:
+            logging.debug("The number of idlejobs or idleslots fufills the requested idleuserjobs, not submitting any glideins")
+            return 0
+        
+        status = ClusterStatus(status_constraint="IsUndefined(Offline)")
+        
+        # Check that running glideins are reporting to the collector
+        running_glidein_jobs = status.GetRunningGlideinJobs()
+        logging.debug("Number of running_glidein_jobs = %i", running_glidein_jobs)
+        running_glideins = status.GetRunningGlideins()
+        logging.debug("Number of running glideins = %i", running_glideins)
+        
+        if ((running_glidein_jobs * .9) > running_glideins):
+            logging.error("I'm guessing glideins are not reporting to the collector, not submitting")
+            return 0
+        
+        # Ok, so now submit until we can't submit any more, or there are less user jobs
+        return min([int(self.config.get("general", "maxqueuedjobs")) - idlejobs, \
+                    idleuserjobs,\
+                    int(self.config.get("general", "MaxIdleGlideins")) - idleslots])
+        
+        
+
+    def SubmitGlideins(self, numSubmit):
+        """
+        Submit numSubmit glideins.
+        
+        @param numSubmit: The number of glideins to submit.
+        """
+        # Substitute values in submit file
+        file = "share/glidein_jobs/job.submit.template"
+
+        # Submit jobs
+        for i in range(numSubmit):
+            self.SingleSubmit(file)
+
+        # Delete the submit file
+
+    def SingleSubmit(self, file):
+        """
+        Submit a single glidein job
+        
+        @param file: The file (string) to submit
+        
+        """
+        
+        # TODO: These options should be moved to a better location
+        options = {"WN_TMP": self.config.get("general", "worker_tmp"), \
+                   "GLIDEIN_HOST": self.condor_config.get("CONDOR_HOST"), \
+                   "GLIDEIN_Site": self.GetClusterUnique()}
+        
+        options_str = ""
+        for key in options.keys():
+            options_str += " -a %s=\"%s\"" % (key, options[key])
+            
+        (stdout, stderr) = RunExternal("condor_submit %s %s" % (file, options_str))
+        logging.debug("stdout: %s" % stdout)
+        logging.debug("stderr: %s" % stderr)
+
+
+
+    def GetClusterUnique(self):
+        """
+        @return: str - The unique identifier for each cluster.  Assuming only 1 cluster for now.
+        """
+        if self.config.has_option("general", "GLIDEIN_Site"):
+            return self.config.get("general", "GLIDEIN_Site")
+        else:
+            return self.condor_config.get("COLLECTOR_NAME")
+
---
+++ trunk/python-lib/campus_factory/Parsers.py
@@ -0,0 +1,144 @@
+import logging
+import xml.sax.handler
+import os
+from popen2 import Popen3
+from select import select
+
+def RunExternal(command, str_stdin=""):
+    """Run an external command 
+    
+    @param command: String of the command to execute
+    @param stdin: String to put put in stdin
+    @return: (str(stdout), str(stderr)) of command
+    Returns the stdout and stderr
+    
+    """
+
+    logging.info("Running external command: %s" % command)
+    popen_inst = Popen3(command, True)
+    logging.debug("stdin = %s" % str_stdin)
+    str_stdout = str_stderr = ""
+    while 1:
+        read_from_child = -1
+        if not popen_inst.tochild.closed:
+            (rlist, wlist, xlist) = select([popen_inst.fromchild, popen_inst.childerr], \
+                                           [popen_inst.tochild], [])
+        else:
+            (rlist, wlist, xlist) = select([popen_inst.fromchild, popen_inst.childerr], [], [])
+
+        if popen_inst.fromchild in rlist:
+            tmpread =  popen_inst.fromchild.read(4096)
+            read_from_child = len(tmpread)
+            str_stdout += tmpread
+        
+        if popen_inst.childerr in rlist:
+            tmpread = popen_inst.childerr.read(4096)
+            read_from_child += len(tmpread)
+            str_stderr += tmpread
+            
+        if popen_inst.tochild in wlist and len(str_stdin) > 0:
+            popen_inst.tochild.write(str_stdin[:min( [ len(str_stdin), 4096])])
+            str_stdin = str_stdin[min( [ len(str_stdin), 4096]):]
+            read_from_child += 1
+        elif popen_inst.tochild in wlist:
+            popen_inst.tochild.close()
+
+        #logging.debug("len(str_stdin) = %i, read_from_child = %i, rlist = %s, wlist = %s", len(str_stdin), read_from_child, rlist, wlist)
+        if popen_inst.poll() != -1 and len(str_stdin) == 0 and (read_from_child == -1 or read_from_child == 0):
+            break
+    
+    logging.debug("Exit code: %i", popen_inst.wait())
+    logging.debug("stdout: %s", str_stdout)
+    logging.debug("strerr: %s", str_stderr)
+    return str_stdout, str_stderr
+
+
+class AvailableGlideins(xml.sax.handler.ContentHandler):
+    
+    # Command to query the collector for available glideins
+    command = "condor_status -avail -const '(IsUndefined(Offline) == True) || (Offline == false)' -format '<glidein name=\"%s\"/>' 'Name'"
+    
+    def __init__(self):
+        pass
+
+    def GetIdle(self):
+        self.idle = 0
+        self.found = False
+
+        # Get the xml from the collector
+        to_parse, stderr = RunExternal(self.command)
+        formatted_to_parse = "<doc>%s</doc>" % to_parse
+
+        # Parse the data
+        try:
+            xml.sax.parseString(formatted_to_parse, self)
+        except xml.sax._exceptions.SAXParseException, inst:
+            logging.error("Error parsing:")
+            logging.error("command = %s" % self.command)
+            logging.error("stderr = %s" % stderr)
+            logging.error("stdout = %s" % to_parse)
+            logging.error("Error: %s - %s" % ( str(inst), inst.args ))
+    
+        if not self.found and (len(stderr) != 0):
+            logging.error("No valid output received from command: %s"% self.command)
+            logging.error("stderr = %s" % stderr)
+            logging.error("stdout = %s" % to_parse)
+            return None
+
+        return self.idle
+    
+    def Run(self):
+        """
+        Generic function for when this class is inherited
+        """
+        return self.GetIdle()
+
+    def startElement(self, name, attributes):
+        if name == "glidein":
+            self.idle += 1
+            self.found = True
+
+
+class IdleGlideins(AvailableGlideins):
+    
+    command = "condor_q -const '(GlideinJob == true) && (JobStatus == 1)' -format '<glidein owner=\"%s\"/>' 'Owner'"
+    
+
+class IdleJobs(AvailableGlideins):
+    
+    command = "condor_q -name %s -const '(GlideinJob =!= true) &&  (JobStatus == 1)' -format '<glidein owner=\"%%s\"/>' 'Owner'"
+
+    def __init__(self, schedd):
+        self.command = self.command % schedd
+
+
+class FactoryID(AvailableGlideins):
+    command = "condor_q -const '(IsUndefined(IsFactory) == FALSE)' -format '<factory id=\"%s\"/>' 'ClusterId'"
+    
+    def startElement(self, name, attributes):
+        if name == "factory":
+            self.factory_id = attributes.getValue("id")
+            self.found = True
+
+    def GetId(self):
+        self.GetIdle()
+        return self.factory_id
+
+class RunningGlideinsJobs(AvailableGlideins):
+    """
+    Gets the number of running glidein jobs (condor_q)
+    """
+    
+    command = "condor_q -const '(GlideinJob == true) && (JobStatus == 2)' -format '<glidein owner=\"%s\"/>' 'Owner'"
+    
+
+class RunningGlideins(AvailableGlideins):
+    """
+    Returns the number of startd's reporting to the collector (condor_status)
+    """
+    
+    command = "condor_status -const '(IsUndefined(IS_GLIDEIN) == FALSE) && (IS_GLIDEIN == TRUE) && (IsUndefined(Offline))' -format '<glidein name=\"%s\"/>' 'Name'"
+    
+    
+    
+    
