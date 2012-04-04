144e3a8d72b8d5c5afe757af0d6cd43fd16410d9
Derek Weitzel <djw8605@gmail.com>
2012-04-03 21:37:50 -0500

    Adding default location of glideinExec

--- python-lib/campus_factory/util/DaemonWrangler.py
+++ python-lib/campus_factory/util/DaemonWrangler.py
@@ -24,21 +24,33 @@ class DaemonWrangler:
             self.daemons = daemons
             
             
-    def Package(self, name=""):    
+    def Package(self, name=""): 
         if name == "":
-            name = "glideinExec.tar.gz"
+            name = "share/glidein_jobs/glideinExec.tar.gz"
         
         # See if the daemons exist
-        if self._CheckDaemons() == False:
+        daemon_paths = self._CheckDaemons()
+        if daemon_paths == None:
             logging.error("Unable to read all daemons, not packaging...")
             raise Exception("Unable to check all daemons")
         
+        tfile = None
         try:
-            tarfile.open()
+            tfile = tarfile.open(name, mode='w:gz')
         except IOError as e:
             logging.error("Unable to open package file %s" % name)
             logging.error(str(e))
+            raise e
             
+        try:
+            for daemon_path in daemon_paths:
+                tfile.add(daemon_path)
+            tfile.close()
+        except IOError as e:
+            logging.error("Error adding files to tar file")
+            logging.error(str(e))
+            raise e
+        
 
 
     def _CheckDaemons(self):
@@ -49,6 +61,7 @@ class DaemonWrangler:
         condor_config = CondorConfig()
         condor_sbin = condor_config.get("SBIN")
         logging.debug("Found SBIN directory = %s" % condor_sbin)
+        daemon_paths = []
         for daemon in self.daemons:
             daemon_path = os.path.join(condor_sbin, daemon)
             logging.debug("Looking for daemon at: %s" % daemon_path)
@@ -60,15 +73,16 @@ class DaemonWrangler:
                 try:
                     fp = open(daemon_path)
                     fp.close()
+                    daemon_paths.append(daemon_path)
                 except IOError as e:
                     logging.error("Unable to open file: %s" % daemon_path)
                     logging.error(str(e))
-                    return False
+                    return None
             else:
                 # If the file doesn't exist
-                return False
+                return None
         
         # Done checking all the daemons
-        return True
+        return daemon_paths
 
 
